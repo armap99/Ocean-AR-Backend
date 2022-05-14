f724f9f31e5bb9213a366d135708eef4ef9ad81c
MarioMog <mamg3108@gmail.com>
2022-05-14 02:38:57 -0500

    chore: run linter-fix

--- src/middlewares/isAuthorized.js
+++ src/middlewares/isAuthorized.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 module.exports.isAuthorized = (req, res, next) => {
-  try {
-    const { authorization } = req.headers
-    const [bearer, token] = authorization.split(' ')
-    jwt.verify(token, 'Workick')
-    next()
-  } catch (error) {
-    res.status(401).json({
-      ok: false,
-      statusCode: 401,
-      message: 'No autorizado',
-      error: error,
-    })
-  }
-}
+    try {
+        const { authorization } = req.headers;
+        const [bearer, token] = authorization.split(" ");
+        jwt.verify(token, "Workick");
+        next();
+    } catch (error) {
+        res.status(401).json({
+            ok: false,
+            statusCode: 401,
+            message: "No autorizado",
+            error
+        });
+    }
+};
