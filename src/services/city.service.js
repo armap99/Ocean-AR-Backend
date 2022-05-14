13bcd1bd122acd94765255d89b114a7badb000a1
Luis Armando Prado <armap1999@hotmail.com>
2022-05-14 09:10:24 -0500

    feat: metodos createCity, updateCity y getCityById

--- src/services/city.service.js
+++ src/services/city.service.js
@@ -8,3 +8,44 @@ module.exports.getAllCities = async () => {
         throw error;
     }
 };
+
+module.exports.getCityById = async (id) => {
+    try {
+        return await City.findByPk(id);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+module.exports.createCity = async (body) => {
+    try {
+        const { name, urlImage, urlMap } = body;
+        return await City.create({
+            Name: name,
+            UrlImage: urlImage,
+            UrlMap: urlMap
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+module.exports.updateCity = async (id, body) => {
+    try {
+        const { name, urlImage, urlMap } = body;
+        return await City.update({
+            Name: name,
+            UrlImage: urlImage,
+            UrlMap: urlMap
+        }, {
+            where: {
+                Id: id
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
