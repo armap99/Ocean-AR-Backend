9732381971c134c622040756bc47451402510012
Luis Armando Prado <armap1999@hotmail.com>
2022-05-14 10:40:20 -0500

    feat: creacion de metodos de user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,35 @@
+const { getUserById, createUser, updateUser } = require("../services/user.service");
+
+module.exports.getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await getUserById(id);
+        res.status(200).json({ data: user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
+
+module.exports.createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await createUser({ name, email, password });
+        res.status(200).json({ data: user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
+
+module.exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, password } = req.body;
+        const user = await updateUser(id, { name, email, password });
+        res.status(200).json({ data: user });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
