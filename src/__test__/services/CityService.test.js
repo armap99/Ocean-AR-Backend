e37bb7dbf501b66fe25824a6ef222a4e3e5b1f8d
Luis Armando Prado <armap1999@hotmail.com>
2022-05-14 10:09:29 -0500

    feat: se agregaron nuevos test para los nuevos metodos

--- src/__test__/services/CityService.test.js
+++ src/__test__/services/CityService.test.js
@@ -1,8 +1,21 @@
-const { getAllCities } = require("../../services/city.service");
+const { getAllCities, createCity, getCityById } = require("../../services/city.service");
 
 describe("CityServices", () => {
     test("1.1)Obtener todas las cities", async () => {
         const prueba = await getAllCities();
-        expect(prueba.length).toBe(0);
+        expect(prueba.length).toBeGreaterThanOrEqual(0);
+    });
+    test("1.2)Agregar una ciudad", async () => {
+        const body = {
+            name: "Ciudad de prueba",
+            urlImage: "https://www.google.com",
+            urlMap: "https://www.google.com"
+        };
+        const prueba = await createCity(body);
+        expect(prueba.Name).toBe(body.name);
+    });
+    test("1.3)Obtener una ciudad por id", async () => {
+        const prueba2 = await getCityById(1);
+        expect(prueba2.Name).toBe("Ciudad de prueba");
     });
 });
