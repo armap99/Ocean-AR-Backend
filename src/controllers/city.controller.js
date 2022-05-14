72582db3dd96258ee0338f5c2a66faf6b3d18fae
Luis Armando Prado <armap1999@hotmail.com>
2022-05-14 10:18:08 -0500

    feat: creacion de metodos restantes

--- src/controllers/city.controller.js
+++ src/controllers/city.controller.js
@@ -1,4 +1,4 @@
-const { getAllCities } = require("../services/city.service");
+const { getAllCities, getCityById, createCity, updateCity } = require("../services/city.service");
 
 module.exports.getAllCities = async (req, res) => {
     try {
@@ -6,6 +6,40 @@ module.exports.getAllCities = async (req, res) => {
         res.status(200).json(cities);
     } catch (error) {
         console.log(error);
-        res.status(500).json(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
+
+module.exports.getCityById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const city = await getCityById(id);
+        res.status(200).json({ data: city });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
+
+module.exports.createCity = async (req, res) => {
+    try {
+        const { name, urlImage, urlMap } = req.body;
+        const city = await createCity({ name, urlImage, urlMap });
+        res.status(200).json({ data: city });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
+    }
+};
+
+module.exports.updateCity = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, urlImage, urlMap } = req.body;
+        const city = await updateCity(id, { name, urlImage, urlMap });
+        res.status(200).json({ data: city });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Server internal error", err: error });
     }
 };
