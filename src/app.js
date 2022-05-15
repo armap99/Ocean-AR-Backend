74490a58019a26b1f750bc1ffaa83265b45000c5
MarioMog <mamg3108@gmail.com>
2022-05-14 21:29:38 -0500

    feat: route health for gh action

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
 app.set("port", process.env.PORT || 5000); // process.env.PORT te obtiene el puerto del hosting
@@ -12,5 +13,8 @@ app.use(cors()); // para poder aceptar peticiones de otros puertos
 app.use("/city", require("./routes/city.routes"));
 app.use("/animal", require("./routes/animal.routes"));
 app.use("/user", require("./routes/user.routes"));
+app.use("/health", (req, res) => {
+    res.status(200).json({ message: "Hello world!!" });
+});
 
 module.exports = app; // exportar aplicacion
