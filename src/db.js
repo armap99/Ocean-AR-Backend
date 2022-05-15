21c18e83ee2eba4ae9dbcf3fbf28b6934b641484
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 23:18:52 -0500

    feat: Se agrega console log

--- src/db.js
+++ src/db.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { DB_URI } = process.env;
+console.log(DB_URI, ">>>>>>>>>>>>>>>");
 const sequelize = new Sequelize(DB_URI);
 
 console.log("DB Connected");
