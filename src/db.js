64e4cf8450a9edb4a7b68c1e144a693a85ab6e73
MarioMog <mamg3108@gmail.com>
2022-05-14 23:51:58 -0500

    feat: se agrega objeto de configuracion

--- src/db.js
+++ src/db.js
@@ -1,7 +1,14 @@
 const { Sequelize } = require("sequelize");
 const { DB_URI } = process.env;
 console.log(DB_URI, ">>>>>>>>>>>>>>>", process.env);
-const sequelize = new Sequelize(DB_URI);
+const sequelize = new Sequelize(DB_URI, {
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 console.log("DB Connected");
 
