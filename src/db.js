deb46b911731b85f57b93fa6930f513e0f2909ef
Moises Hernandez Del toro <xbox8980@live.com.mx>
2022-05-14 23:13:48 -0500

    refactor: Nuevas variablles de entorno

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-const { DB_URI } = require("./dbConfig");
+const { DB_URI } = process.env;
 const sequelize = new Sequelize(DB_URI);
 
 console.log("DB Connected");
